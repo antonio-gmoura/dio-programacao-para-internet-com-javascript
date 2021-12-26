908c924d963764715844051afa7d04595c6a2911
Antonio Moura <antonio.gmoura@gmail.com>
2021-12-26 01:02:32 -0400

    Condicionais, laço de repetição e Date

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
+/*
 //alert("meu primeiro js")
 
 var nome = "Rafael Galleani"
 //alert("Bem vindo " + nome);
 console.log("Bem vindo " + nome.toLocaleUpperCase());
+*/
+
+/*
+var idade = prompt("Qual sua idade?")
+if (idade >= 18) {
+    alert("maior de idade")
+}else{
+    alert("menor de idade")
+}
+*/
+
+/*
+var count = 0;
+while (count <= 5){
+    console.log("while: " + count);
+    count ++
+}
+
+for (count = 0; count <= 5; count++) {
+    console.log("for:" + count);
+}
+*/
+
+var d = new Date();
+console.log(d);
